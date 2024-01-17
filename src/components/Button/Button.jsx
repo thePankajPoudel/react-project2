2954445ad6deead30f2c7838a91da883858e285a
Pankaj Poudel <poudelpankaj9@gmail.com>
2024-01-17 13:00:18 +0545

    this is second

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,17 @@
+
+import styles from "./Button.module.css";
+const Button = ({isOutline, icon , text, ...rest}) => {
+ // const {isOutline, icon , text} = props;
+  
+    return (
+    <button 
+    {...rest}
+     className={isOutline ? styles.outline_btn: styles.primary_btn}>
+     
+      {icon}
+      {text}
+    </button>
+  );
+};
+
+export default Button;
